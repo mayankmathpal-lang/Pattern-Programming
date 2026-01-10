7db03354b8b68c13e1caa24dbf4659c80f2d0fb9
Mayank Mathpal <mayankmathpal939@gmail.com>
2026-01-10 06:57:11 +0530

    first commit

---
+++ Launchpattern1.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Launchpattern1 {
+
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+           BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+           int n= Integer.parseInt(br.readLine());
+           for (int i =1; i<=n; i++) {
+			for (int j =1; j<=n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
